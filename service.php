8eef72e478fde5fe066c3a6f00334cf0ae7dc7a6
Adam McLain <pdxadam@gmail.com>
2025-02-10 07:28:21 -0800

    All main functions created.

--- service.php
+++ service.php
@@ -5,32 +5,78 @@
     }
     $db = getConn();
     if ($db === false){
-        echo("Error: data connection failed");
+        error_log("Error 01: could not connect to database");
+        echo("Error 01: data connection failed");
         die();
     }
     $rq = filter_input(INPUT_POST, "rq", FILTER_VALIDATE_INT);
     switch($rq){
         case 10:
-            //login?
+            //login
             //u (username), p(password), a(appID)
-            $u = filter_input(INPUT_POST, "u", FILTER_DEFAULT);
-            $p = filter_input(INPUT_POST, "p", FILTER_DEFAULT);
-            $app = filter_input(INPUT_POST, "app", FILTER_VALIDATE_INT);
-            checkLogin($u, $p, $app, $db);
+            try{
+                $u = filter_input(INPUT_POST, "u", FILTER_DEFAULT);
+                $p = filter_input(INPUT_POST, "p", FILTER_DEFAULT);
+                $app = filter_input(INPUT_POST, "app", FILTER_VALIDATE_INT);
+                $results = checkLogin($u, $p, $db);
+                if ($results === true){
+                    $_SESSION['appID'] == $app;//storing as session so this one can only do this app
+                    $appData = getApp($db);
+                }
+            else{
+                session_destroy(); //kill the session if we ran into an unexpected error
+                echo($results[1]);
+                break;
+            }
+            }
+            catch (Exception $e){
+                error_log($e);
+            }
+            
+            
             break;
         case 20:
             //connect app
             //validate user authentication.authorization first
+            //make sure the app is correct (i.e. it is an app that I accept)
             break;
             
         case 30:
             //get data
+            $result = getApp($db);
+            echo($result[1]);
+
             break;
         case 40: 
             //put data
+            //d data
+            //if the app/owner exists, we'll update it. 
+            //if not, we'll create it.
+            //need to validate the data structure as it comes in.
+            $data = filter_input(INPUT_POST, "d", FILTER_DEFAULT);
+            $result = putData($data, $db);
+            echo($result[1]);
+            
             break;
         case 50:
             //create account
+            //Right now, any old schmuck can create an email
+            $u = filter_input(INPUT_POST, 'username', FILTER_DEFAULT);
+            $p = filter_input(INPUT_POST, 'password', FILTER_DEFAULT);
+            $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+            if ($email == false){
+                error_log("Error 500.  invalid email " . $email);
+                echo ("Error 500: invalid email");
+                session_destroy();
+                break;
+            }
+            $result = createUser($u, $p, $email, $db);
+            session_destroy();
+            echo($result[1]);
+            //if we create the user, we'll tell them success.  If it fails, we'll tell them so. 
+            //if success, they need to proceed to login.
+            //if failure, they need to try again, or contact support.
+
             break;
         
 
@@ -38,8 +84,24 @@
 
     }
     $db = null; //close the database every time.
-    function checkLogin($u, $p, $a, $db = null){
-        if ($db = null){
+
+    // ----- end of main code, functions after--------------------------------
+    
+    function userExists($u, $db){ // returns true if the username is taken. False if it is not
+        $sql = $db->prepare("SELECT count(*) as myCount FROM tblUser WHERE userName = :u");
+        $sql->bindValue(":u", $u);
+        if ($sql->execute()){
+                $row = $sql->fetch(PDO::FETCH_ASSOC);
+                return $row['myCount'] == 1;
+        }
+        else{
+            return false;
+        }
+    }
+    function checkLogin($u, $p, $db = null){
+        //returns true and sets $_SESSION[pkUser] if success
+        //returns array of [false, "error message"] if failed
+        if ($db == null){
             $db = getConn();
         }
         $sql = $db->prepare("SELECT * FROM tblUser WHERE username = :username");
@@ -48,16 +110,101 @@
             $rows = $sql->fetchAll(PDO::FETCH_ASSOC);
             if (count($rows) == 1){
                 //there is one user
-                $_SESSION['pkUser'] = $rows[0]['pkUser'];
-                echo("Success");
-
-
+                if (password_verify($p, $rows[0]['password'])){
+                    //check if we need to rehash
+                    if (password_needs_rehash($rows[0]['password'], PASSWORD_DEFAULT)){
+                        //then we need to update the password to what it already is.
+                    }
+                    $_SESSION['pkUser'] = $rows[0]['pkUser'];
+                    
+                    return(true); //now it needs to request the right app
+                }
             }
             else{
                 session_destroy();
-                echo("Error: Authentication failed");
+                error_log("Error 101: invalid authentication request: no user found for user '" . $u . "'");
+                return [false,"Error 101: Authentication failed"];
 
             }
         }
+        else{
+            session_destroy();
+            error_log("Error 102: authentication failed for user '" . $u . "'");
+            return [false, "Error 102: Authentication failed"];
+        }
         
+    }
+    function createUser($u, $p, $email, $db){
+        if (userExists($u, $db)){
+            error_log("Error 502: Attempt to create duplicate user '" . $u . "'");
+            return [false, "Error 502: Username is taken"];
+        }
+        $sql = $db->prepare("INSERT INTO tbluser (username, email, password) VALUES(:u, :e, :p)");
+        $sql->bindValue(":u", $u);
+        $sql->bindValue(":p", $p);
+        $sql->bindValue(":e", $email);
+        if ($sql->execute()){         
+            return [true, "User created. Please login."];
+        }
+        else{
+            error_log("Error 503: Failed creating user '" . $u . "' with email '" . $email . "'");
+            return [false, "Error 503: Error creating user"];
+        }
+
+
+    }
+    function getApp($db){
+        $sql = $db->prepare("SELECT (* FROM tblData WHERE fkApp = :app AND fkOwner = :user");
+        $sql->bindValue(":app", $_SESSION['appID']);
+        $sql->bindValue(":user", $_SESSION['pkUser']);
+        if ($sql->execute()){
+            $rows = $sql->fetchAll(PDO::FETCH_ASSOC);
+            //should always be only one
+            if (count($rows) == 1){
+                return([true,$rows[0]['data']]);
+
+            }
+            else if (count($rows) == 0){
+                //they don't have this app yet. Insert it?
+                
+                return [false, "Error 300: App not yet connected"];
+            }
+            else{
+                //they've logged in, but they don't have this app
+                error_log("Error 302: Incorrect data row count. app " . $_SESSION['appID'] . " user " . $_SESSION['pkUser']);
+                return [false, "Error 301: Incorrect data count: " . count($rows)];
+            }
+
+        }
+    }
+    function putData($data, $db){
+        $res = getApp($db);
+        if ($res[0] == false){
+            if (str_starts_with($res[1], "Error 300")){
+                //there's no data there, we need to create it.
+                $sql = $db->prepare("INSERT INTO tblData (fkApp, fkOwner, data) VALUES (:app, :owner, :data);");
+                $sql->bindValue(":app", $_SESSION['appID']);
+                $sql->bindValue(":owner", $_SESSION['pkUser']);
+                $sql->bindValue(":data", $data);
+                if ($sql->execute()){
+                    return [true, "Success: App data created"];
+                }
+                else{
+                    error_log("Error 401: user: " . $_SESSION['pkUser'] . " app: " . $_SESSION['appID'] . " Data: " . $data);
+                    return [false, "Error 401: App data creation failed."];
+                }
+                 }
+            else{
+                error_log("Error 402: user: " . $_SESSION['pkUser'] . " app: " . $_SESSION['appID'] . " Data: " . $data);
+                return [false, "Error 402: User Data structure is corrupt. Contact support"];
+            }
+        }
+        else{
+            //we need to update the data in the table
+            $sql = $db->prepare("UPDATE tblData SET data = :data WHERE fkOwner = :owner AND fkApp = :app");
+            $sql->bindValue(":app", $_SESSION['appID']);
+            $sql->bindValue(":owner", $_SESSION['pkUser']);
+            $sql->bindValue(":data", $data);
+        } 
+    
     }
